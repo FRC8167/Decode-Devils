a0beb23e45ad1c45e55c6ff9fbb21405519ff7cd
emark <emarks0825@gmail.com>
2025-10-13 18:36:11 -0400

    MethodSequencerTest added (Just experimenting)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cogintilities/MethodSequencerTest.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.Cogintilities;
+
+import androidx.annotation.NonNull;
+
+import org.firstinspires.ftc.teamcode.SubSystems.SpinStatesSingleton;
+import org.firstinspires.ftc.teamcode.SubSystems.Spindexer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MethodSequencerTest implements TeamConstants {
+
+    private TimedTimer timer;
+
+    private final List<Runnable> methods;
+
+    private boolean done;
+
+    private double timeDelay = 0;
+
+
+    public MethodSequencerTest() {
+        methods = new ArrayList<>();
+        timer = new TimedTimer();
+        done = true;
+    }
+
+    public void setTimeDelay(double time) {
+        timeDelay = time;
+    }
+
+    public void addMethods(Runnable... methods) {
+        this.methods.addAll(Arrays.asList(methods));
+    }
+
+    public void runMethods(Runnable... methods) {
+        addMethods(methods);
+        start();
+    }
+
+//    public <T> void runMethod(Consumer<T> consumer, T parameter) {
+//        consumer.accept(parameter);
+//    }
+
+    public void runMethod(Runnable runnable) {
+        runnable.run();
+    }
+
+    public void start() {
+        done = false;
+    }
+    public void stop() {
+        done = true;
+    }
+
+    public void update() {
+        if (!done && timer.isDone()) {
+            if (!methods.isEmpty()) {
+                methods.remove(0).run();
+                timer = new TimedTimer(timeDelay);
+            } else {
+                done = true;
+            }
+        }
+    }
+
+
+
+}
