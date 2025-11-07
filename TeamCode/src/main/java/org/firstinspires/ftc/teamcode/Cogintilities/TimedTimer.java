6b66155a4a536ac40c4676ee77aa6425feda32d1
emark <emarks0825@gmail.com>
2025-11-07 17:42:10 -0500

    TimedTimer freeze functionality added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cogintilities/TimedTimer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cogintilities/TimedTimer.java
@@ -4,6 +4,9 @@ public class TimedTimer {
     private double initialTime;
     private double duration;
 
+    private double freezeTime;
+    private boolean frozen = false;
+
     public TimedTimer(double timeInSeconds) {
         initialTime = System.currentTimeMillis();
         duration = timeInSeconds*1000;
@@ -17,26 +20,50 @@ public class TimedTimer {
     public void startNewTimer(double timeInSeconds) {
         initialTime = System.currentTimeMillis();
         duration = timeInSeconds*1000;
+        frozen = false;
+        freezeTime = Double.NaN;
     }
 
     public void reset() {
         initialTime = System.currentTimeMillis();
+        frozen = false;
+        freezeTime = Double.NaN;
     }
 
+    public void freeze() {
+        if (frozen) return;
+        frozen = true;
+        freezeTime = System.currentTimeMillis();
+    }
+
+    public void unfreeze() {
+        if (!frozen) return;
+        frozen = false;
+        initialTime += System.currentTimeMillis() - freezeTime;
+        freezeTime = Double.NaN;
+    }
+
+    private double time() {
+        return (frozen ? freezeTime : System.currentTimeMillis());
+    }
+
+
+
     public boolean isDone() {
-        return (initialTime + duration <= System.currentTimeMillis());
+        return (initialTime + duration <= time());
     }
 
     public double getRemainingTime() {
-        return Math.max(0, (initialTime + duration) - System.currentTimeMillis());
+        return Math.max(0, (initialTime + duration) - time());
     }
 
     public double getProportionCompleted() {
         if (duration == 0) return -1;
-        return (duration - (Math.max(0, (initialTime + duration) - System.currentTimeMillis())))/duration;
+        return (duration - (Math.max(0, (initialTime + duration) - time())))/duration;
     }
 
     public double getElapsedTime() {
-        return initialTime - System.currentTimeMillis();
+        return initialTime - time();
     }
+
 }
