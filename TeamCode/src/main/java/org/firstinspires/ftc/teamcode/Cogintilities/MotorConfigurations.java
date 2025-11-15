b08b29f3dabfd50f603a711ee2a80da7fe376ede
emark <emarks0825@gmail.com>
2025-11-14 23:15:48 -0500

    Motor gearing values added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cogintilities/MotorConfigurations.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cogintilities/MotorConfigurations.java
@@ -4,23 +4,25 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
 public enum MotorConfigurations {
-    GOBILDA_6000RPM(20,     6000),
-    GOBILDA_1620RPM(103.8,  1620),
-    GOBILDA_1150RPM(145.1,  1150),
-    GOBILDA_435RPM (384.5,  435),
-    GOBILDA_312RPM (537.7,  312),
-    GOBILDA_223RPM (751.8,  223),
-    GOBILDA_117RPM (1425.1, 117),
-    GOBILDA_84RPM  (1993.6, 84),
-    GOBILDA_60RPM  (2786.2, 60),
-    GOBILDA_43RPM  (3895.9, 43),
-    GOBILDA_30RPM  (5281.1, 30);
+    GOBILDA_6000RPM(20,     6000, 1),
+    GOBILDA_1620RPM(103.8,  1620, 3.7),
+    GOBILDA_1150RPM(145.1,  1150, 5.2),
+    GOBILDA_435RPM (384.5,  435,  13.7),
+    GOBILDA_312RPM (537.7,  312,  19.2),
+    GOBILDA_223RPM (751.8,  223,  26.9),
+    GOBILDA_117RPM (1425.1, 117,  50.9),
+    GOBILDA_84RPM  (1993.6, 84,   71.2),
+    GOBILDA_60RPM  (2786.2, 60,   99.5),
+    GOBILDA_43RPM  (3895.9, 43,   139),
+    GOBILDA_30RPM  (5281.1, 30,   188);
     private final double ticksPerRev;
     private final double maxRPM;
+    private final double gearing;
 
-    MotorConfigurations(double ticksPerRev, double maxRPM) {
+    MotorConfigurations(double ticksPerRev, double maxRPM, double gearing) {
         this.ticksPerRev = ticksPerRev;
         this.maxRPM = maxRPM;
+        this.gearing = gearing;
     }
 
     public double getTicksPerRev() {
@@ -31,6 +33,10 @@ public enum MotorConfigurations {
         return maxRPM;
     }
 
+    public double getGearing() {
+        return gearing;
+    }
+
     public void configureMotor(DcMotor motor) {
         configureMotor(motor, this);
     }
@@ -39,6 +45,7 @@ public enum MotorConfigurations {
         MotorConfigurationType newConfig = motor.getMotorType().clone();
         newConfig.setTicksPerRev(motorConfig.getTicksPerRev());
         newConfig.setMaxRPM(motorConfig.getMaxRPM());
+        newConfig.setGearing(motorConfig.getGearing());
         motor.setMotorType(newConfig);
     }
 }
