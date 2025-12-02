4e35af92f49d722218a70e516708450cedbcf52d
RobotAdmin <frc8167@gmail.com>
2025-12-02 15:49:53 -0500

    SpinnerTesting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTestSpinner.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.Cogintilities.BetterMotor;
+import org.firstinspires.ftc.teamcode.Cogintilities.MotorInformation;
+import org.firstinspires.ftc.teamcode.Cogintilities.TeamConstants;
+import org.firstinspires.ftc.teamcode.SubSystems.Spinner;
+
+@TeleOp(name="TeleOpTestSpinner", group="Competition")
+public class TeleOpTestSpinner extends LinearOpMode implements TeamConstants {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        Spinner spinner = new Spinner(hardwareMap.get(Servo.class, "spinServo"), 0.5, 0, 1, true);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if (gamepad2.yWasPressed()) {
+                spinner.setCenteredPositionDegrees(0);
+            } else if (gamepad2.bWasPressed()) {
+                spinner.rotateBy(10);
+            } else if (gamepad2.aWasPressed()) {
+
+            } else if (gamepad2.xWasPressed()) {
+                spinner.rotateBy(-10);
+            }
+
+            telemetry.addData("Pos: ", spinner.getCenteredPositionDegrees());
+            telemetry.update();
+        }
+    }
+}
